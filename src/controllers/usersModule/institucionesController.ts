c51e99fb7727f07e3fce8062b11ce9e486c0adaa
Agus <agusrodriguez2456@gmail.com>
2022-08-10 18:52:24 -0300

    habilitar instituciones finished

--- src/controllers/usersModule/institucionesController.ts
+++ src/controllers/usersModule/institucionesController.ts
@@ -106,7 +106,7 @@ const institucionesController = {
             const { nombre, cuit, razonSocial, email, domicilio, telefono, idRol, link } = req.body
 
             //crear usuario
-            const nuevoUsuario = await Usuario.create({ password: null, email: email, telefono: telefono })
+            const nuevoUsuario = await Usuario.create({ password: null, email: email, telefono: telefono, rolActivo: idRol })
             const idNuevoUsuario = nuevoUsuario['dataValues']['id']
 
             await UsuarioRol.create({ fk_idUsuario: idNuevoUsuario, fk_idRol: idRol })
