17de9acff01079cf0feff7aacc4602bef77237be
Agus <agusrodriguez2456@gmail.com>
2022-08-16 20:12:35 -0300

    baja convenio

--- src/router/obrasSocialesModule/conveniosRoute.ts
+++ src/router/obrasSocialesModule/conveniosRoute.ts
@@ -5,7 +5,7 @@ const router = Router()
 
 router.get("/convenios/:idPersonaJuridica", conveniosController.getConvenios);
 router.get("/convenios/:idPersonaJuridica/:idConvenio", conveniosController.getConvenioById);
-router.delete("/convenio/eliminar/:idPersonaJuridica/:idConvenio", conveniosController.bajaConvenio);
+router.delete("/convenios/eliminar/:idPersonaJuridica/:idConvenio", conveniosController.bajaConvenio);
 router.post("/convenios/agregar/:idPersonaJuridica", conveniosController.agregarConvenio);
 //obtener todos los tratamientos que cubre esa obra social
 router.get("/tratamientosObraSocialConvenio/:idObraSocial/:idPersonaJuridica", conveniosController.getTratamientosConvenio);
