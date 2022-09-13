ac55b9f523883e3212fd9ebb59af34f4cd983a61
Agus <agusrodriguez2456@gmail.com>
2022-09-13 17:50:04 -0300

    removed roles superUsuario and paciente

--- src/controllers/tratamientosModule/tratamientoPacienteController.ts
+++ src/controllers/tratamientosModule/tratamientoPacienteController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import moment from 'moment'
+//import moment from 'moment'
 import TratamientoParticular from '../../models/entities/obrasSocialesModule/tratamientoParticular';
 import TratamientoPaciente from '../../models/entities/tratamientosModule/tratamientoPaciente';
 import PersonaJuridicaPaciente from '../../models/entities/usersModule/personaJuridicaPaciente';
