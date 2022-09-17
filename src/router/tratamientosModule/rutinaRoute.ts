0ff5f5af2ab9fc2fd1ead754bd17fff3deb8f8ad
Agus <agusrodriguez2456@gmail.com>
2022-09-17 15:27:52 -0300

    added fk_idProfesional to rutina

--- src/router/tratamientosModule/rutinaRoute.ts
+++ src/router/tratamientosModule/rutinaRoute.ts
@@ -3,7 +3,7 @@ import rutinaController from "../../controllers/tratamientosModule/rutinaControl
 
 const router = Router()
 
-router.post("/agregarRutina/:idTratamientoPaciente", rutinaController.createRutina);
+router.post("/agregarRutina/:idTratamientoPaciente/:idProfesional", rutinaController.createRutina);
 router.get("/rutina/:idRutina", rutinaController.getRutinaById);
 router.put("/editarRutina/:idRutina", rutinaController.editarRutina);
 router.delete("/finalizarRutina/:idRutina", rutinaController.finalizarRutina);
