cd14d176ba2193ef39fd41e2b401b3d3c3857567
Agus <agusrodriguez2456@gmail.com>
2022-09-27 17:17:26 -0300

    mostrarRutinaBandera

--- src/controllers/tratamientosModule/rutinaController.ts
+++ src/controllers/tratamientosModule/rutinaController.ts
@@ -166,7 +166,8 @@ const rutinaController = {
                 contadorRacha: rutinaToFind['dataValues']['contadorRacha'],
                 dateLastRacha: rutinaToFind['dataValues']['dateLastRacha'],
                 rutinaEjercicios: rutinaEjerciciosRes,
-                jsonRutina: rutinaToFind['dataValues']['jsonRutina'] ? rutinaToFind['dataValues']['jsonRutina'] : null
+                jsonRutina: rutinaToFind['dataValues']['jsonRutina'] ? rutinaToFind['dataValues']['jsonRutina'] : null,
+                mostrarRutinaBandera: rutinaToFind['dataValues']['mostrarRutinaBandera'] ? rutinaToFind['dataValues']['mostrarRutinaBandera'] : null
             }
 
             res.status(200).json(responseFinal)
