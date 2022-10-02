a0f9c67c0cf77d007b2ef3d5b71c300c8cec4579
Agus <agusrodriguez2456@gmail.com>
2022-10-02 16:54:50 -0300

    db config

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -4,7 +4,7 @@ const db = new Sequelize('heroku_fe8c7e5eb423ca6', 'ba4faf146889f3', '71ce21dc',
     host: 'us-cdbr-east-06.cleardb.net',
     dialect: 'mysql',
     pool: {
-        max: 100,
+        max: 300,
         min: 0,
         acquire: 60000,
         idle: 30000
