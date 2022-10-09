01ddc7fb9a385f7ef778ea42c9058570d8bf71d0
Agus <agusrodriguez2456@gmail.com>
2022-10-09 15:20:41 -0300

    fixed heroku dates

--- src/helpers/verifyAlarmas.ts
+++ src/helpers/verifyAlarmas.ts
@@ -42,7 +42,7 @@ export default async () => {
                 }
                 if (!recordatorios[i]['dataValues']['habilitado'] || !recordatorios[i]['dataValues']['activo']) { continue }
 
-                if (hours[dateNowHours.split(":")[0]].includes(dateRecordatorioHours.split(":")[0]) &&
+                if ((hours[dateNowHours.split(":")[0]].includes(dateRecordatorioHours.split(":")[0]) || dateRecordatorioHours.split(":")[0] == dateNowHours.split(":")[0]) &&
                     dateRecordatorioHours.split(":")[1] == dateNowHours.split(":")[1]) {
 
                     console.log("Horario alcanzado!!!!!!!");
