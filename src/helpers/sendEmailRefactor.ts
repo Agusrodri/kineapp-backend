e42cd92452e3e0da7316b33ca0ccf7b0bd64277f
Agus <agusrodriguez2456@gmail.com>
2022-10-10 20:35:30 -0300

    html templates

--- src/helpers/sendEmailRefactor.ts
+++ src/helpers/sendEmailRefactor.ts
@@ -16,7 +16,41 @@ export default (nuevoLink: string, email: string, subject: string, htmlText: str
                 from: '4devteam.utn@gmail.com',
                 to: email,
                 subject: subject,
-                html: `<a href=${nuevoLink}>${htmlText}</a>` // html body
+                //html: `<a href=${nuevoLink}>${htmlText}</a>` // html body
+                html: `<style>
+                .email-content{
+                  background-color: #FFF;
+                  width: 100%;
+                  height: 400px;
+                  display: flex;
+                  flex-direction: column;
+                  justify-content: center;
+                  align-items: center;
+              }
+              .logo-kineapp{
+                  width: 200px;
+              }
+              h1{
+                  font-size: 24px;
+                  color: #5b5b5b;
+                  font-weight: 800;
+                  margin-top: 40px;
+              }
+              a{
+                  margin-top: 20px;
+                  font-size: 14px;
+                  text-decoration: none;
+                  color: #0093b0;
+              }
+              a:hover{
+                  cursor: pointer;
+              }
+              </style>
+              <div class="email-content">
+                <img src="http://api-kineapp.herokuapp.com/logo-kineapp-nombre.png" alt="" class="logo-kineapp">
+                <h1>${subject}</h1>
+                <a href="${nuevoLink}">${htmlText}</a>
+              </div>`
             };
 
             transporter.sendMail(mailOptions, (error, info) => {
