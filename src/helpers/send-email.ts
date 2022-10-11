c690b6a3bd1caed36a7e0817ccae54b8fc677424
Agus <agusrodriguez2456@gmail.com>
2022-10-10 21:07:27 -0300

    html templates finished

--- src/helpers/send-email.ts
+++ src/helpers/send-email.ts
@@ -16,7 +16,7 @@ export default (nuevoLink: string, email: string) => {
                 from: '4devteam.utn@gmail.com',
                 to: email,
                 subject: "Verificación cuenta kineapp",
-                html: `<div class="email-content" style="background-color: #FFF;width: 100%;height: 400px;" align="center"><img src="http://api-kineapp.herokuapp.com/logo-kineapp-nombre.png" alt="" class="logo-kineapp" style="width: 180px;height:200px;"><h1 style="font-size: 24px;color: #5b5b5b;font-weight: 800;margin-top: 20px;">Verificación de cuenta</h1><a style="margin-top: 20px;font-size: 14px;text-decoration: none;color: #0093b0;" href="${nuevoLink}">Click aquí para verificar tu cuenta de kineapp</a></div>` // html body
+                html: `<div class="email-content" style="background-color: #FFF;width: 100%;height: 400px;" align="center"><img src="http://api-kineapp.herokuapp.com/logo-kineapp-nombre.png" alt="" class="logo-kineapp" style="width: 180px;height:200px;"><h1 style="font-size: 24px;color: #5b5b5b;font-weight: 800;margin-top: 20px;">Verificación cuenta kineapp</h1><a style="margin-top: 20px;font-size: 14px;text-decoration: none;color: #0093b0;" href="${nuevoLink}">Click aquí para verificar tu cuenta de kineapp</a></div>` // html body
             };
 
             transporter.sendMail(mailOptions, (error, info) => {
