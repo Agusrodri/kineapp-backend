e1df076d68eca0e4ad860b187b792f094586e542
Agus <agusrodriguez2456@gmail.com>
2022-11-07 17:41:54 -0300

    removed middleware

--- src/router/usersModule/profesionalesRoute.ts
+++ src/router/usersModule/profesionalesRoute.ts
@@ -8,7 +8,7 @@ const router = Router()
 router.get("/profesionales/:idPersonaJuridica", [validarJWT], profesionalesController.getProfesionales);
 router.get("/profesionales/:idPersonaJuridica/:idProfesional", [validarJWT], profesionalesController.getProfesionalById);
 router.post("/profesionales/crearSinUsuario/:idPersonaJuridica", [validarJWT], profesionalesController.crearProfesionalSinUsuario);
-router.put("/profesionales/validar/:idUsuario", [validarJWT], profesionalesController.validarProfesional);
+router.put("/profesionales/validar/:idUsuario", profesionalesController.validarProfesional);
 router.post("/profesionales/test/buscarUsuario/:idPersonaJuridica", [validarJWT], profesionalesController.buscarUsuario);
 router.post("/profesionales/crearConUsuario/:idUsuario/:idPersonaJuridica", [validarJWT], profesionalesController.crearProfesionalConUsuario);
 router.get("/tipoDNI/all", profesionalesController.getTiposDni);
